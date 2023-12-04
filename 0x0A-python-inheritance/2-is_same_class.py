5b19db01e2d97b3ed0e9a27a2873308a5f4a666e
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-12-04 10:03:05 -0800

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+
+
+def is_same_class(obj, a_class):
+    """returns True if object is intance and false if not
+
+
+    Args:
+        obj: The object to check
+        a_class: The class to CHECK IF same with obj
+    """
+
+    if type(obj) == a_class:
+        return True
+    return False
+
