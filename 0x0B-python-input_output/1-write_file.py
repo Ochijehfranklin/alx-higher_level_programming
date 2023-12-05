2874694df1d013547119a9f1861cbb816291bb57
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-12-05 05:05:18 -0800

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -14,5 +14,5 @@ def write_file(filename="", text=""):
             the number of charsacters
     """
 
-    with open(filename, mode="w", encoding="utf-8") as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return (f.write(text))
