df3d3bd91a549142be120bc87b99aa70d933bc09
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-12-11 17:35:12 -0800

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,15 +90,10 @@ class Rectangle(Base):
 
     def display(self):
          """Print the Rectangle using the `#` character."""
-        if self.width == 0 or self.height == 0:
-            print("")
-            return
-
-        [print("") for y in range(self.y)]
-        for h in range(self.height):
-            [print(" ", end="") for x in range(self.x)]
-            [print("#", end="") for w in range(self.width)]
-            print("")
+         for w in range(self.y):
+             print()
+         for i in range(self.height):
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
