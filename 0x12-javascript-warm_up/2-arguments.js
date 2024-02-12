63fa5d63d2de9dfe61d97d93ac68d4678e5f8e46
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-02-12 16:35:33 +0100

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const argCount = args.length;
+
+if (argCount === 0) 
+{
+	console.log('No argument');
+}
+else if (argCount === 1)
+{
+	console.log('Argument found')
+}
+else
+{
+	console.log('Arguments found');
+}
