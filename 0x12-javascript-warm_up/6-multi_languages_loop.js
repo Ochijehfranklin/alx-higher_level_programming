c1c4090d91d6f0246553023bee7a111d8747a4a5
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-02-13 03:11:53 +0100

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const val = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const val = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let x = 0; x < val.length; x++) {
   console.log(val[x]);
