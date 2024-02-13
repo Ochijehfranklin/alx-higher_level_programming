1cf60b517a47e04cb0a1290f1f0ff6ad48fbc688
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-02-13 01:40:36 +0100

    value of argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 
-const arg1 = process.argv[2] === process.argv[1] ? 'No argument' : process.argv[2];
-
-console.log(arg1);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
