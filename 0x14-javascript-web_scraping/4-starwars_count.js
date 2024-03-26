6a24b85c23d7ea893550ef06c3a6b29061e86d59
ochijeh Franklin <ochijehfranklin@gmail.com>
2024-03-26 20:56:22 +0100

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 
 const characterId = 18; // Wedge Antilles' character ID
 const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(apiUrl, { json: true }, (err, res, body) => {
-    if (err) {
-        console.error('Error:', err);
-        return;
-    }
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
 
-    const moviesWithWedge = body.results.filter(movie =>
-        movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
+  const moviesWithWedge = body.results.filter(movie =>
+    movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  );
 
-    console.log(`${moviesWithWedge.length}`);
+  console.log(`${moviesWithWedge.length}`);
 });
-
