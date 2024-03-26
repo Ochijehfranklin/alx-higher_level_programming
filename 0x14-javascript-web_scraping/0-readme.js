0b434b549fefed289790ff851bea205078c13a89
ochijeh Franklin <ochijehfranklin@gmail.com>
2024-03-26 10:53:49 +0100

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Directly use the file path from the command line arguments
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+    if (err) console.error("An error occurred:", err);
+    else console.log(data);
+});
+
